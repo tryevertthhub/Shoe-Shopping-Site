55f316e89cfafcba4e9be48689998ea8a03333dc
tryevertthhub <tryevertth@gmail.com>
2024-07-15 21:49:39 +0900

    feat: add Avatar component

---
+++ frontend/src/components/Avatar.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+const Avatar = () => {
+    return (
+      <img
+        className="rounded-full"
+        height={30}
+        width={30}
+        src="./images/placeholder.jpg"
+        alt="image"
+      />
+    );
+  };
+  
+  export default Avatar;
+  
