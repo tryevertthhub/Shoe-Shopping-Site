aeef2cda89f00b783c9e40968731b3a35b1eb925
blockdevguru <tryevertthhub>
2024-07-22 23:29:46 +0900

    feat: add Footer component in Frontend

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return <div className=" w-full stiky bottom-0 h-[80px] bg-blue-600 items-center">
+  
+      <footer className="w-full h-full textwhite bg-blue-600 body-font">
+  
+        <div className="bg-blue-600 h-full">
+          <div className="container px-5 py-4 mx-auto">
+            <p className="text-sm lg:text-3xl textwhite capitalize  text-center text-white">© 2024 All rights reserved </p>
+          </div>
+        </div>
+      </footer>
+  
+    </div>
+  };
+  
+  export default Footer;
+  
