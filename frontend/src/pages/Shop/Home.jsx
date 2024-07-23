ad339b331c96ab181671ff0c7d3e9c82d9ea3ed8
blockdevguru <tryevertthhub>
2024-07-23 14:56:08 +0900

    feat: add Home component in Fronted

---
+++ frontend/src/pages/Shop/Home.jsx
@@ -0,0 +1,14 @@
+import Table from "../../components/Table"
+import Header from "../../components/Header";
+const Home = () => {
+
+  return (
+    <>
+     <Header />
+      <Table />
+    </>
+
+  );
+};
+
+export default Home;
